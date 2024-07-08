c2b2abf6274c02df627b78a9dc3fff3fd969169a
Bara Yuda <520463+barayuda@users.noreply.github.com>
2024-07-08 15:08:12 +0700

    feat: init project

---
+++ server/src/common/swagger/constants.ts
@@ -0,0 +1,5 @@
+export const SWAGGER_API_ROOT = 'api/docs';
+export const SWAGGER_API_NAME = 'Nest Next Boilerplate v1 API';
+export const SWAGGER_API_DESCRIPTION = 'Basic docs for API in version 1';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
+export const SERVER_URL = 'http://localhost:4000';
